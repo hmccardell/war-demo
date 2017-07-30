e7ce3091b4fb5936f71f020b3b9a8c0d26e6388e
Hayes McCardell II <hmccardell@catalyte.io>
2017-07-30 15:23:11 -0700

    uncommented the system display line

--- src/com/hmccardell/entities/GameState.java
+++ src/com/hmccardell/entities/GameState.java
@@ -55,7 +55,7 @@ public class GameState {
         systemTotals.append("Total cards in system: " + total + " [ ");
         systemTotals.append(individualTotals.toString());
         systemTotals.append("]");
-        //System.out.println(systemTotals.toString().trim());
+        System.out.println(systemTotals.toString().trim());
 
     }
 
